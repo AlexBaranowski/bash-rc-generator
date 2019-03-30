a635587fcf7b20241162e34238979ca0051d5152
Alex Baranowski <alex@euro-linux.com>
2019-03-30 01:51:53 +0100

    Site is getting cleaner, history 70% done

---
+++ src/custom.js
@@ -0,0 +1,55 @@
+/* 
+ * Please note that I'm not frontend developer. This code might be dead simple and verbose.
+ * If you think that there is room for improvement - pull request are open ;).
+*/
+$( function() {
+  $("#accordion").accordion();
+  //disable toggles
+  $('#history-append-checkbox').bootstrapToggle('off')  
+  $('#history-shared-checkbox').bootstrapToggle('off')  
+
+});
+// History
+$( "#history-file-size-example" ).click(function() {
+  $("#history-file-size-input").val("2000")
+});
+$( "#history-size-example" ).click(function() {
+  $("#history-size-input").val("2000")
+});
+// There might be smarter way to set this with name of id, but 
+$( "#history-histcontrol-example-none" ).click(function() {
+  $("#history-histcontrol-input").val("")
+});
+$( "#history-ignore-example" ).click(function() {
+  $("#history-ignore-input").val("cd*:false:history:htop:ls*:ll*:la:l:popd:pushd*:reset:top:true")
+});
+$( "#history-histcontrol-example-ignoreboth" ).click(function() {
+  $("#history-histcontrol-input").val("ignoreboth")
+});
+$( "#history-histcontrol-example-ignorespace" ).click(function() {
+  $("#history-histcontrol-input").val("ignorespace")
+});
+$( "#history-histcontrol-example-ignoredups" ).click(function() {
+  $("#history-histcontrol-input").val("ignoredups")
+});
+$( "#history-histcontrol-example-erasedups" ).click(function() {
+  $("#history-histcontrol-input").val("erasedups")
+});
+$( "#history-time-format-example" ).click(function() {
+  $("#history-time-format-input").val("%Y-%m-%d %T ")
+});
+$( "#history-file-example" ).click(function() {
+  $("#history-file-input").val("~/.my_bash_history")
+});
+// There should be smarter solution, but a) I don't want to learn js b) Most of js code is stackoverflowed
+
+/*function histctrl-toogles() {
+  console.log("Ala ma kota")
+    var clicked = $(this);
+  $('.histctrl-toogles').not(clicked).bootstrapToogle("off");
+};*/
+function histctrltoogles() {
+  console.log("Ala ma kota");
+  var clicked = $(this);
+  $('.histctrl-toogles').prop('checked', false).change();
+}
